a42dd29b3052d96185918da27b6f1e5bf9f3a0a1
milicapavlovic96 <pavlovic380@gmail.com>
2020-01-22 16:47:07 +0100

    controller fixed

--- modules/custom/custom_module/src/Controller/ProductController.php
+++ modules/custom/custom_module/src/Controller/ProductController.php
@@ -4,17 +4,25 @@ namespace Drupal\custom_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class ProductController extends ControllerBase {
+
+class ProductController extends ControllerBase  {
 
     public function page(){
-        
-        $query = \Drupal::entityQuery('node');
-        $query->condition('type', 'product');
-        $items = $query->execute();
 
+        $query= \Drupal::entityQuery('node');
+        $nids = $query->condition('type', 'product')->execute();
+
+        $items = \Drupal\node\Entity\Node::loadMultiple($nids);
+
+
+        foreach($items as $item){
+            $product=\Drupal::entityTypeManager()->getViewBuilder('node')->view($item);
+            $products[]=render($product);
+        }
+        
         return array(
             '#theme' => 'product_list',
-            '#items' => $items,
+            '#items' => $products,
             '#title' => 'Product list'
         );
     }
