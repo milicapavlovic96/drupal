1e3e25e8921166f8f14bfc63976ac0629513446e
milicapavlovic96 <pavlovic380@gmail.com>
2020-01-23 10:00:00 +0100

    commit

--- modules/custom/custom_module/src/Controller/ProductController.php
+++ modules/custom/custom_module/src/Controller/ProductController.php
@@ -4,16 +4,14 @@ namespace Drupal\custom_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-
 class ProductController extends ControllerBase  {
 
     public function page(){
 
         $query= \Drupal::entityQuery('node');
         $nids = $query->condition('type', 'product')->execute();
-
+        
         $items = \Drupal\node\Entity\Node::loadMultiple($nids);
-
 
         foreach($items as $item){
             $product=\Drupal::entityTypeManager()->getViewBuilder('node')->view($item);
