b0c155092151e34bf3255b38c4e4c7cc64300295
milicapavlovic96 <pavlovic380@gmail.com>
2020-01-27 15:21:06 +0100

    search_api added

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 /**
  * @file
- * serves all page requests on a Drupal installation.
- *dasdasdasdasdasdsa
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- */  
+ */
 
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 $autoloader = require_once 'autoload.php';
-       
+
 $kernel = new DrupalKernel('prod', $autoloader);
 
 $request = Request::createFromGlobals();
